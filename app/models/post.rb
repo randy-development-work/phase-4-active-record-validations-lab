f9965627efd70ad7b4234975e4c06d3c3103f6b7
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-03-16 16:35:40 +0300

    commit tree 526c53f382b83c39dd2c9d62269f09559430c4bc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+
+
+    # validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    # validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validates :category, inclusion: ['Fiction', 'Non-Fiction']
+
+    validate :sufficient_clickbait_y
+
+    def sufficient_clickbait_y
+        unless title.present? && title.match?(/(Secret)|(Won't Believe)|(Top \d)|(Guess)/i)
+            errors.add(:title, "Only clickbait-y Titles Allowed!")
+        end
+    end
 end
